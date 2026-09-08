355e71fc0c3cc639553d89d6df0d14497da557f6
zuromu <ahmadhoesin7@gmail.com>
2026-09-08 18:00:31 +0700

    feat: add view and url routing for the projects section

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 def show_main(request):
@@ -22,3 +22,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_projects(request):
+    context = {
+        "name": "Ahmad Hoesin",
+        "projects_list": Project.objects.all(),
+    }
+    return render(request, "projects.html", context)
